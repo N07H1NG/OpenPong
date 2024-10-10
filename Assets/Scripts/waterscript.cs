b37c2adb108c80bacd9c5dddaba81f8957e5c6e7
N07H1NG <nikitos09012004@mail.ru>
2024-10-11 00:00:50 +0300

    sfx water

--- Assets/Scripts/waterscript.cs
+++ Assets/Scripts/waterscript.cs
@@ -16,5 +16,11 @@ public class waterscript : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(ball.transform.position.x,transform.position.y,transform.position.z);
+        if (ball.transform.position.y<transform.position.y){
+            audioComp.enabled = false;
+        }
+        else{
+            audioComp.enabled = true;
+        }
     }
 }
