9f3657cf91487d71e50f57e4f0c22d55a96051d0
N07H1NG <nikitos09012004@mail.ru>
2024-10-16 16:44:49 +0300

    minor fixes

--- Assets/Scripts/ScoreTextScript.cs
+++ Assets/Scripts/ScoreTextScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.Mathematics;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
